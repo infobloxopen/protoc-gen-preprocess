b1339fc68e019cfc0979bd67219e5d12afdf52ab
binjip978 <binjip978@gmail.com>
2021-09-02 10:42:51 +0300

    generate Preprocess function header

--- main.go
+++ main.go
@@ -62,10 +62,27 @@ func generate(plugin *protogen.Plugin) *pluginpb.CodeGeneratorResponse {
 			fileName := protoFile.GeneratedFilenamePrefix + ".pb.preprocess.go"
 			g := plugin.NewGeneratedFile(fileName, ".")
 			g.P("package ", protoFile.GoPackageName)
+
+			for _, message := range protoFile.Messages {
+				if message.Desc.IsMapEntry() {
+					continue
+				}
+				generateProto3Message(g, message)
+			}
 		}
 	}
 
 	return plugin.Response()
+}
+
+func generateProto3Message(g *protogen.GeneratedFile, message *protogen.Message) {
+	typeName := camelCase(string(message.Desc.Name()))
+	fmt.Fprintf(os.Stderr, "typeName: %s\n", typeName)
+
+	g.P(`func (m *`, typeName, `) Preprocess() error {`)
+	g.P()
+	g.P("return nil")
+	g.P("}")
 }
 
 func getMessageOptions(message *protogen.Message) *preprocess.PreprocessMessageOptions {
@@ -102,4 +119,51 @@ func getFieldOptions(field *protogen.Field) *preprocess.PreprocessFieldOptions {
 		return nil
 	}
 	return opts
+}
+
+func camelCase(s string) string {
+	if s == "" {
+		return ""
+	}
+	t := make([]byte, 0, 32)
+	i := 0
+	if s[0] == '_' {
+		// Need a capital letter; drop the '_'.
+		t = append(t, 'X')
+		i++
+	}
+	// Invariant: if the next letter is lower case, it must be converted
+	// to upper case.
+	// That is, we process a word at a time, where words are marked by _ or
+	// upper case letter. Digits are treated as words.
+	for ; i < len(s); i++ {
+		c := s[i]
+		if c == '_' && i+1 < len(s) && isASCIILower(s[i+1]) {
+			continue // Skip the underscore in s.
+		}
+		if isASCIIDigit(c) {
+			t = append(t, c)
+			continue
+		}
+		// Assume we have a letter now - if not, it's a bogus identifier.
+		// The next word is a sequence of characters that must start upper case.
+		if isASCIILower(c) {
+			c ^= ' ' // Make it a capital letter.
+		}
+		t = append(t, c) // Guaranteed not lower case.
+		// Accept lower case sequence that follows.
+		for i+1 < len(s) && isASCIILower(s[i+1]) {
+			i++
+			t = append(t, s[i])
+		}
+	}
+	return string(t)
+}
+
+func isASCIILower(c byte) bool {
+	return 'a' <= c && c <= 'z'
+}
+
+func isASCIIDigit(c byte) bool {
+	return '0' <= c && c <= '9'
 }
